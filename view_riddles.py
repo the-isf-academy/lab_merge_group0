79c20a87f21c0aa3842bb3dca3e337c090e86c1e
Dubai <4618001@student.isf.edu.hk>
2022-10-20 11:19:00 +0800

    practise without pulling

--- view_riddles.py
+++ view_riddles.py
@@ -2,7 +2,7 @@ from all_riddles import riddles_json
 
 
 def main():
-    riddles_list = riddles_json['riddles']
+    riddles_list = riddles_json['riddle']
 
     for riddle in riddles_list:
         print(riddle["id"])
